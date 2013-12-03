c8322e8046d862da1e14cd70656457edd1163285
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2013-12-03 13:02:42 -0700

    Initial commit showing Jetty 9.1 + JSP in embedded mode

---
+++ src/main/java/org/eclipse/jetty/demo/Main.java
@@ -0,0 +1,189 @@
+//
+//  ========================================================================
+//  Copyright (c) 1995-2013 Mort Bay Consulting Pty. Ltd.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
+
+package org.eclipse.jetty.demo;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.apache.jasper.servlet.JspServlet;
+import org.eclipse.jetty.server.ConnectionFactory;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.servlet.DefaultServlet;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.log.JavaUtilLog;
+import org.eclipse.jetty.util.log.Log;
+
+/**
+ * Example of using JSP's with embedded jetty and not requiring
+ * all of the overhead of a WebAppContext
+ */
+public class Main
+{
+    // Resource path pointing to where the WEBROOT is
+    private static final String WEBROOT_INDEX = "/webroot/";
+
+    public static void main(String[] args) throws Exception
+    {
+        int port = 8080;
+        LoggingUtil.config();
+        Log.setLog(new JavaUtilLog());
+
+        Main main = new Main(port);
+        main.start();
+        main.waitForInterrupt();
+    }
+
+    private static final Logger LOG = Logger.getLogger(Main.class.getName());
+
+    private int port;
+    private Server server;
+    private URI serverURI;
+
+    public Main(int port)
+    {
+        this.port = port;
+    }
+
+    public URI getServerURI()
+    {
+        return serverURI;
+    }
+
+    public void start() throws Exception
+    {
+        server = new Server();
+        ServerConnector connector = new ServerConnector(server);
+        connector.setPort(port);
+        server.addConnector(connector);
+
+        URL indexUri = this.getClass().getResource(WEBROOT_INDEX);
+        if (indexUri == null)
+        {
+            throw new FileNotFoundException("Unable to find resource " + WEBROOT_INDEX);
+        }
+
+        // Points to wherever /webroot/ (the resource) is
+        URI baseUri = indexUri.toURI();
+
+        // Establish Scratch directory for the servlet context (used by JSP compilation)
+        File tempDir = new File(System.getProperty("java.io.tmpdir"));
+        File scratchDir = new File(tempDir.toString(),"embedded-jetty-jsp");
+
+        if (!scratchDir.exists())
+        {
+            if (!scratchDir.mkdirs())
+            {
+                throw new IOException("Unable to create scratch directory: " + scratchDir);
+            }
+        }
+
+        // Set JSP to use Standard JavaC always
+        System.setProperty("org.apache.jasper.compiler.disablejsr199","false");
+
+        // Setup the basic application "context" for this application at "/"
+        // This is also known as the handler tree (in jetty speak)
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        context.setAttribute("javax.servlet.context.tempdir",scratchDir);
+        context.setResourceBase(baseUri.toASCIIString());
+        server.setHandler(context);
+
+        // Set Classloader of Context to be sane (needed for JSTL)
+        // JSP requires a non-System classloader, this simply wraps the
+        // embedded System classloader in a way that makes it suitable
+        // for JSP to use
+        ClassLoader jspClassLoader = new URLClassLoader(new URL[0], this.getClass().getClassLoader());
+        context.setClassLoader(jspClassLoader);
+
+        // Add JSP Servlet (must be named "jsp")
+        ServletHolder holderJsp = new ServletHolder("jsp",JspServlet.class);
+        holderJsp.setInitOrder(0);
+        holderJsp.setInitParameter("logVerbosityLevel","DEBUG");
+        holderJsp.setInitParameter("fork","false");
+        holderJsp.setInitParameter("xpoweredBy","false");
+        holderJsp.setInitParameter("compilerTargetVM","1.7");
+        holderJsp.setInitParameter("compilerSourceVM","1.7");
+        holderJsp.setInitParameter("keepgenerated","true");
+        context.addServlet(holderJsp,"*.jsp");
+        context.addServlet(holderJsp,"*.jspf");
+        context.addServlet(holderJsp,"*.jspx");
+
+        // Add Example of mapping jsp to path spec
+        ServletHolder holderAltMapping = new ServletHolder("foo.jsp", JspServlet.class);
+        holderAltMapping.setForcedPath("/test/foo/foo.jsp");
+        context.addServlet(holderAltMapping,"/test/foo/");
+
+        // Add Default Servlet (must be named "default")
+        ServletHolder holderDefault = new ServletHolder("default",DefaultServlet.class);
+        LOG.info("Base URI: " + baseUri);
+        holderDefault.setInitParameter("resourceBase",baseUri.toASCIIString());
+        holderDefault.setInitParameter("dirAllowed","true");
+        context.addServlet(holderDefault,"/");
+
+        // Start Server
+        server.start();
+
+        // Show server state
+        if (LOG.isLoggable(Level.FINE))
+        {
+            LOG.fine(server.dump());
+        }
+
+        // Establish the Server URI
+        String scheme = "http";
+        for (ConnectionFactory connectFactory : connector.getConnectionFactories())
+        {
+            if (connectFactory.getProtocol().equals("SSL-http"))
+            {
+                scheme = "https";
+            }
+        }
+        String host = connector.getHost();
+        if (host == null)
+        {
+            host = "localhost";
+        }
+        int port = connector.getLocalPort();
+        serverURI = new URI(String.format("%s://%s:%d/",scheme,host,port));
+        LOG.info("Server URI: " + serverURI);
+    }
+
+    public void stop() throws Exception
+    {
+        server.stop();
+    }
+
+    /**
+     * Cause server to keep running until it receives a Interrupt.
+     * <p>
+     * Interrupt Signal, or SIGINT (Unix Signal), is typically seen as a result of a kill -TERM {pid} or Ctrl+C
+     */
+    public void waitForInterrupt() throws InterruptedException
+    {
+        server.join();
+    }
+}
---
+++ src/main/resources/webroot/index.jsp
@@ -0,0 +1,22 @@
+<html>
+<head>
+  <title>Embedded Jetty: JSP Examples</title>
+  <link href="static/main.css" media="all" rel="stylesheet" type="text/css" />
+</head>
+<body>
+  <h1>Embedded Jetty: JSP Examples</h1>
+  <p>
+    Examples of JSP within Embedded Jetty.
+  </p>
+  <ul>
+    <li><a href="test/dump.jsp">JSP 1.2 embedded java</a><br/>
+    <li><a href="test/bean1.jsp">JSP 1.2 Bean demo</a><br/>
+    <li><a href="test/tag.jsp">JSP 1.2 BodyTag demo</a><br/>
+    <li><a href="test/tag2.jsp">JSP 2.0 SimpleTag demo</a><br/>
+    <li><a href="test/tagfile.jsp">JSP 2.0 Tag File demo</a><br/>
+    <li><a href="test/expr.jsp?A=1">JSP 2.0 Tag Expression</a><br/>
+    <li><a href="test/jstl.jsp">JSTL Expression</a><br/>
+    <li><a href="test/foo/">Mapping to &lt;jsp-file&gt;</a><br/>
+  </ul>
+</body>
+</html>
